9a5c754c10106916052bc420623304f9c3ef8481
Jake <ljangwon@gmail.com>
2021-04-04 01:01:40 +0900

    fix

--- todo/application/logs/log-2021-04-03.php
+++ todo/application/logs/log-2021-04-03.php
@@ -65,3 +65,97 @@ DEBUG - 2021-04-03 17:33:28 --> Helper loaded: url_helper
 DEBUG - 2021-04-03 17:33:28 --> File loaded: application/views/todo/list_v.php
 DEBUG - 2021-04-03 17:33:28 --> Final output sent to browser
 DEBUG - 2021-04-03 17:33:28 --> Total execution time: 0.0718
+ERROR - 2021-04-03 17:55:57 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\todo\system\core\Common.php 257
+DEBUG - 2021-04-03 17:55:57 --> Config Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:55:57 --> URI Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Router Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 17:55:57 --> Output Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Security Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Input Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:55:57 --> Language Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:55:57 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:55:57 --> File loaded: application/views/welcome_message.php
+DEBUG - 2021-04-03 17:55:57 --> Final output sent to browser
+DEBUG - 2021-04-03 17:55:57 --> Total execution time: 0.0280
+ERROR - 2021-04-03 17:58:30 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\todo\system\core\Common.php 257
+DEBUG - 2021-04-03 17:58:30 --> Config Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:58:30 --> URI Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Router Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 17:58:30 --> Output Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Security Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Input Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:58:30 --> Language Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:58:30 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:58:30 --> File loaded: application/views/welcome_message.php
+DEBUG - 2021-04-03 17:58:30 --> Final output sent to browser
+DEBUG - 2021-04-03 17:58:30 --> Total execution time: 0.0215
+ERROR - 2021-04-03 17:58:34 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\todo\system\core\Common.php 257
+DEBUG - 2021-04-03 17:58:34 --> Config Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:58:34 --> URI Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Router Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 17:58:34 --> Output Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Security Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Input Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:58:34 --> Language Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:58:34 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:58:34 --> File loaded: application/views/welcome_message.php
+DEBUG - 2021-04-03 17:58:34 --> Final output sent to browser
+DEBUG - 2021-04-03 17:58:34 --> Total execution time: 0.0211
+ERROR - 2021-04-03 17:59:49 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\todo\system\core\Common.php 257
+DEBUG - 2021-04-03 17:59:49 --> Config Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:59:49 --> URI Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Router Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 17:59:49 --> Output Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Security Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Input Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:59:49 --> Language Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:59:49 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:59:49 --> File loaded: application/views/welcome_message.php
+DEBUG - 2021-04-03 17:59:49 --> Final output sent to browser
+DEBUG - 2021-04-03 17:59:49 --> Total execution time: 0.0211
+DEBUG - 2021-04-03 18:01:11 --> Config Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Hooks Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 18:01:11 --> URI Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Router Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 18:01:11 --> Output Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Security Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Input Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 18:01:11 --> Language Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Loader Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 18:01:11 --> Controller Class Initialized
+DEBUG - 2021-04-03 18:01:11 --> File loaded: application/views/welcome_message.php
+DEBUG - 2021-04-03 18:01:11 --> Final output sent to browser
+DEBUG - 2021-04-03 18:01:11 --> Total execution time: 0.0228
