a8845b7ed5f67b39a8d3fbb58d9e37578f0e2b88
Jake <ljangwon@gmail.com>
2021-04-04 00:54:17 +0900

    first

---
+++ bbs/application/logs/log-2021-04-03.php
@@ -0,0 +1,117 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-03 17:36:10 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\bbs\system\core\Common.php 257
+DEBUG - 2021-04-03 17:36:10 --> Config Class Initialized
+DEBUG - 2021-04-03 17:36:10 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:36:10 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:36:10 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:36:10 --> URI Class Initialized
+DEBUG - 2021-04-03 17:36:10 --> Router Class Initialized
+ERROR - 2021-04-03 17:36:10 --> 404 Page Not Found --> main
+ERROR - 2021-04-03 17:36:26 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\bbs\system\core\Common.php 257
+DEBUG - 2021-04-03 17:36:26 --> Config Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:36:26 --> URI Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> Router Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 17:36:26 --> Output Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> Security Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> Input Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> CRSF cookie Set
+DEBUG - 2021-04-03 17:36:26 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:36:26 --> Language Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:36:26 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:36:26 --> Database Driver Class Initialized
+ERROR - 2021-04-03 17:36:26 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\dev\bin\xampp\htdocs\ci\bbs\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2021-04-03 17:36:29 --> Severity: Warning  --> mysql_connect(): ����� ���������κ��� ����� ���� ������� ���߰ų�, ȣ��Ʈ�κ��� ����� ���� ����� �������ϴ�.
+ D:\dev\bin\xampp\htdocs\ci\bbs\system\database\drivers\mysql\mysql_driver.php 73
+ERROR - 2021-04-03 17:36:29 --> Unable to connect to the database
+DEBUG - 2021-04-03 17:36:29 --> Language file loaded: language/korean/db_lang.php
+ERROR - 2021-04-03 17:37:08 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\bbs\system\core\Common.php 257
+DEBUG - 2021-04-03 17:37:08 --> Config Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:37:08 --> URI Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Router Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 17:37:08 --> Output Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Security Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Input Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> CRSF cookie Set
+DEBUG - 2021-04-03 17:37:08 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:37:08 --> Language Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:37:08 --> Database Driver Class Initialized
+ERROR - 2021-04-03 17:37:08 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\dev\bin\xampp\htdocs\ci\bbs\system\database\drivers\mysql\mysql_driver.php 73
+DEBUG - 2021-04-03 17:37:08 --> Session Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> Helper loaded: string_helper
+DEBUG - 2021-04-03 17:37:08 --> A session cookie was not found.
+DEBUG - 2021-04-03 17:37:08 --> Session routines successfully run
+DEBUG - 2021-04-03 17:37:08 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:37:08 --> File loaded: application/views/welcome_message.php
+DEBUG - 2021-04-03 17:37:08 --> Final output sent to browser
+DEBUG - 2021-04-03 17:37:08 --> Total execution time: 0.1580
+ERROR - 2021-04-03 17:37:50 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\bbs\system\core\Common.php 257
+DEBUG - 2021-04-03 17:37:50 --> Config Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:37:50 --> URI Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Router Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Output Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Security Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Input Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> CRSF cookie Set
+DEBUG - 2021-04-03 17:37:50 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:37:50 --> Language Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:37:50 --> Database Driver Class Initialized
+ERROR - 2021-04-03 17:37:50 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\dev\bin\xampp\htdocs\ci\bbs\system\database\drivers\mysql\mysql_driver.php 73
+DEBUG - 2021-04-03 17:37:50 --> Session Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Helper loaded: string_helper
+DEBUG - 2021-04-03 17:37:50 --> Session routines successfully run
+DEBUG - 2021-04-03 17:37:50 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Model Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Model Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> Helper loaded: form_helper
+ERROR - 2021-04-03 17:37:50 --> Severity: Notice  --> Undefined index: logged_in D:\dev\bin\xampp\htdocs\ci\bbs\application\views\header_v.php 23
+DEBUG - 2021-04-03 17:37:50 --> File loaded: application/views/header_v.php
+DEBUG - 2021-04-03 17:37:50 --> Pagination Class Initialized
+DEBUG - 2021-04-03 17:37:50 --> DB Transaction Failure
+ERROR - 2021-04-03 17:37:50 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'WHERE 1=1  AND board_pid = '0' ORDER BY board_id DESC' at line 1
+DEBUG - 2021-04-03 17:37:50 --> Language file loaded: language/korean/db_lang.php
+ERROR - 2021-04-03 17:38:21 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\bbs\system\core\Common.php 257
+DEBUG - 2021-04-03 17:38:21 --> Config Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:38:21 --> URI Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Router Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Output Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Security Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Input Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> CRSF cookie Set
+DEBUG - 2021-04-03 17:38:21 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:38:21 --> Language Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:38:21 --> Database Driver Class Initialized
+ERROR - 2021-04-03 17:38:21 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\dev\bin\xampp\htdocs\ci\bbs\system\database\drivers\mysql\mysql_driver.php 73
+DEBUG - 2021-04-03 17:38:21 --> Session Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Helper loaded: string_helper
+DEBUG - 2021-04-03 17:38:21 --> Session routines successfully run
+DEBUG - 2021-04-03 17:38:21 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Model Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Model Class Initialized
+DEBUG - 2021-04-03 17:38:21 --> Helper loaded: form_helper
+ERROR - 2021-04-03 17:38:21 --> Severity: Notice  --> Undefined index: logged_in D:\dev\bin\xampp\htdocs\ci\bbs\application\views\header_v.php 23
+DEBUG - 2021-04-03 17:38:21 --> File loaded: application/views/header_v.php
+DEBUG - 2021-04-03 17:38:21 --> File loaded: application/views/footer_v.php
+DEBUG - 2021-04-03 17:38:21 --> Final output sent to browser
+DEBUG - 2021-04-03 17:38:21 --> Total execution time: 0.0779
---
+++ todo/application/logs/log-2021-04-03.php
@@ -0,0 +1,67 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-03 17:22:17 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\todo\system\core\Common.php 257
+DEBUG - 2021-04-03 17:22:17 --> Config Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:22:17 --> URI Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Router Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> No URI present. Default controller set.
+DEBUG - 2021-04-03 17:22:17 --> Output Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Security Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Input Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:22:17 --> Language Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:22:17 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:22:17 --> File loaded: application/views/welcome_message.php
+DEBUG - 2021-04-03 17:22:17 --> Final output sent to browser
+DEBUG - 2021-04-03 17:22:17 --> Total execution time: 0.1250
+ERROR - 2021-04-03 17:23:09 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\todo\system\core\Common.php 257
+DEBUG - 2021-04-03 17:23:09 --> Config Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:23:09 --> URI Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Router Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Output Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Security Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Input Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:23:09 --> Language Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:23:09 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Database Driver Class Initialized
+ERROR - 2021-04-03 17:23:09 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\dev\bin\xampp\htdocs\ci\todo\system\database\drivers\mysql\mysql_driver.php 73
+DEBUG - 2021-04-03 17:23:09 --> Model Class Initialized
+DEBUG - 2021-04-03 17:23:09 --> Model Class Initialized
+DEBUG - 2021-04-03 17:23:10 --> Helper loaded: url_helper
+DEBUG - 2021-04-03 17:23:10 --> File loaded: application/views/todo/list_v.php
+DEBUG - 2021-04-03 17:23:10 --> Final output sent to browser
+DEBUG - 2021-04-03 17:23:10 --> Total execution time: 0.1566
+ERROR - 2021-04-03 17:33:28 --> Severity: Notice  --> Only variable references should be returned by reference D:\dev\bin\xampp\htdocs\ci\todo\system\core\Common.php 257
+DEBUG - 2021-04-03 17:33:28 --> Config Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Hooks Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Utf8 Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> UTF-8 Support Enabled
+DEBUG - 2021-04-03 17:33:28 --> URI Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Router Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Output Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Security Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Input Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-03 17:33:28 --> Language Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Loader Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Helper loaded: date_helper
+DEBUG - 2021-04-03 17:33:28 --> Controller Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Database Driver Class Initialized
+ERROR - 2021-04-03 17:33:28 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\dev\bin\xampp\htdocs\ci\todo\system\database\drivers\mysql\mysql_driver.php 73
+DEBUG - 2021-04-03 17:33:28 --> Model Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Model Class Initialized
+DEBUG - 2021-04-03 17:33:28 --> Helper loaded: url_helper
+DEBUG - 2021-04-03 17:33:28 --> File loaded: application/views/todo/list_v.php
+DEBUG - 2021-04-03 17:33:28 --> Final output sent to browser
+DEBUG - 2021-04-03 17:33:28 --> Total execution time: 0.0718
